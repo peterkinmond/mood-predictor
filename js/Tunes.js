41869547f353ec808fa77618d51a20d2fe5a84ef
Peter Kinmond <peterkinmond@gmail.com>
2012-02-12 09:10:42 -0800

    Move js function into separate file

--- js/Tunes.js
+++ js/Tunes.js
@@ -1,9 +1,22 @@
 (function($) {
-
-
+    var time_of_day_count = 0;
 
     $(function() {
+        $("input[name='time_of_day']").change(function(e) {
+            alterMood(this, time_of_day_count * -1); // Reset count
+            time_of_day_count = 0;
 
+            if ($(this).val() == 'morning') {
+               time_of_day_count -= 1;
+            }
+            else if($(this).val() == 'afternoon') {
+               time_of_day_count += 0;
+            }
+            else if($(this).val() == 'evening') {
+               time_of_day_count -= 3;
+            }
+
+            alterMood(this, time_of_day_count);
+        });
     });
-
 })(jQuery);
