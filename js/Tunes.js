e99ab5f5e0be48ae1c737f2560a27ee299b55c7d
Peter Kinmond <peterkinmond@gmail.com>
2012-02-12 20:14:58 -0800

    add dogs question

--- js/Tunes.js
+++ js/Tunes.js
@@ -1,5 +1,6 @@
 (function($) {
-    var time_of_day_count = 0;
+    var time_of_day_count = 0,
+        number_of_dogs_count = 0;
 
     $(function() {
         $("input[name='time_of_day']").change(function(e) {
@@ -7,16 +8,33 @@
             time_of_day_count = 0;
 
             if ($(this).val() == 'morning') {
-               time_of_day_count -= 1;
+                time_of_day_count -= 1;
             }
-            else if($(this).val() == 'afternoon') {
-               time_of_day_count += 0;
+            else if ($(this).val() == 'afternoon') {
+                time_of_day_count += 0;
             }
-            else if($(this).val() == 'evening') {
-               time_of_day_count -= 3;
+            else if ($(this).val() == 'evening') {
+                time_of_day_count -= 3;
             }
 
             alterMood(this, time_of_day_count);
+        });
+
+        $("input[name='number_of_dogs']").change(function(e) {
+            alterMood(this, number_of_dogs_count * -1); // Reset count
+            number_of_dogs_count = 0;
+
+            if ($(this).val() == 'zero') {
+                number_of_dogs_count += 0;
+            }
+            else if ($(this).val() == 'one') {
+                number_of_dogs_count += 0;
+            }
+            else if ($(this).val() == 'two') {
+                number_of_dogs_count -= 3;
+            }
+
+            alterMood(this, number_of_dogs_count);
         });
     });
 })(jQuery);
