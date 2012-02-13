f81fc78433661daae1e23a82a200df2eaac49d73
Peter Kinmond <peterkinmond@gmail.com>
2012-02-12 20:27:18 -0800

    Refactored JS to be less horrible

--- js/Tunes.js
+++ js/Tunes.js
@@ -6,34 +6,14 @@
         $("input[name='time_of_day']").change(function(e) {
             alterMood(this, time_of_day_count * -1); // Reset count
             time_of_day_count = 0;
-
-            if ($(this).val() == 'morning') {
-                time_of_day_count -= 1;
-            }
-            else if ($(this).val() == 'afternoon') {
-                time_of_day_count += 0;
-            }
-            else if ($(this).val() == 'evening') {
-                time_of_day_count -= 3;
-            }
-
+            time_of_day_count += parseInt($(this).val());
             alterMood(this, time_of_day_count);
         });
 
         $("input[name='number_of_dogs']").change(function(e) {
             alterMood(this, number_of_dogs_count * -1); // Reset count
             number_of_dogs_count = 0;
-
-            if ($(this).val() == 'zero') {
-                number_of_dogs_count += 0;
-            }
-            else if ($(this).val() == 'one') {
-                number_of_dogs_count += 0;
-            }
-            else if ($(this).val() == 'two') {
-                number_of_dogs_count -= 3;
-            }
-
+            number_of_dogs_count += parseInt($(this).val());
             alterMood(this, number_of_dogs_count);
         });
     });
