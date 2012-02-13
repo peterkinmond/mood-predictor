1e994c3fff1073b108056e2a36c8fb8f7e0413c2
Peter Kinmond <peterkinmond@gmail.com>
2012-02-12 20:38:28 -0800

    add office temp question

--- js/Tunes.js
+++ js/Tunes.js
@@ -1,6 +1,7 @@
 (function($) {
     var time_of_day_count = 0,
-        number_of_dogs_count = 0;
+        number_of_dogs_count = 0,
+        office_temp = 0;
 
     $(function() {
         $("input[name='time_of_day']").change(function(e) {
@@ -15,6 +16,13 @@
             number_of_dogs_count = 0;
             number_of_dogs_count += parseInt($(this).val());
             alterMood(this, number_of_dogs_count);
+        });
+
+        $("input[name='office_temp']").change(function(e) {
+            alterMood(this, office_temp * -1); // Reset count
+            office_temp = 0;
+            office_temp += parseInt($(this).val());
+            alterMood(this, office_temp);
         });
     });
 })(jQuery);
