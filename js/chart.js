2116890bb432699c7422560c59412ad57802ff2e
Peter Kinmond <peterkinmond@gmail.com>
2012-02-15 22:52:40 -0800

    Add question

--- js/chart.js
+++ js/chart.js
@@ -1,7 +1,8 @@
 (function($) {
     var time_of_day_count = 0,
         number_of_dogs_count = 0,
-        office_temp = 0;
+        office_temp = 0,
+        cups_of_coffee = 0;
 
     $(function() {
         $("input[name='time_of_day']").change(function(e) {
@@ -23,6 +24,13 @@
             office_temp = 0;
             office_temp += parseInt($(this).val());
             alterMood(this, office_temp);
+        });
+
+        $("input[name='cups_of_coffee']").change(function(e) {
+            alterMood(this, cups_of_coffee * -1); // Reset count
+            cups_of_coffee = 0;
+            cups_of_coffee += parseInt($(this).val());
+            alterMood(this, cups_of_coffee);
         });
 
         drawChart(0);
