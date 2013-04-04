9f40c3a2e7977d74f825282943b37f1ca9c74cee
Peter Kinmond <peterkinmond@gmail.com>
2013-04-03 20:55:28 -0700

    Refactor event handlers to use common function and data attribute

--- js/chart.js
+++ js/chart.js
@@ -1,38 +1,20 @@
-(function($) {
-    var time_of_day_count = 0,
-        number_of_dogs_count = 0,
-        office_temp = 0,
-        cups_of_coffee = 0;
+$(function() {
+    setQuestionListener('office_temp');
+    setQuestionListener('cups_of_coffee');
+    setQuestionListener('time_of_day');
+    setQuestionListener('number_of_dogs');
 
-    $(function() {
-        $("input[name='time_of_day']").change(function(e) {
-            alterMood(this, time_of_day_count * -1); // Reset count
-            time_of_day_count = 0;
-            time_of_day_count += parseInt($(this).val());
-            alterMood(this, time_of_day_count);
-        });
+    drawChart(36 * -2);
+});
 
-        $("input[name='number_of_dogs']").change(function(e) {
-            alterMood(this, number_of_dogs_count * -1); // Reset count
-            number_of_dogs_count = 0;
-            number_of_dogs_count += parseInt($(this).val());
-            alterMood(this, number_of_dogs_count);
-        });
-
-        $("input[name='office_temp']").change(function(e) {
-            alterMood(this, office_temp * -1); // Reset count
-            office_temp = 0;
-            office_temp += parseInt($(this).val());
-            alterMood(this, office_temp);
-        });
-
-        $("input[name='cups_of_coffee']").change(function(e) {
-            alterMood(this, cups_of_coffee * -1); // Reset count
-            cups_of_coffee = 0;
-            cups_of_coffee += parseInt($(this).val());
-            alterMood(this, cups_of_coffee);
-        });
-
-        drawChart(36 * -2);
+function setQuestionListener(questionText) {
+    $("input[name='" + questionText + "']").change(function(e) {
+        var prevAnswer = $("#chart").data(questionText);
+        var newAnswer = parseInt($(this).val());
+        if (prevAnswer !== undefined) {
+            alterMood(this, prevAnswer * -1); // Undo current value
+        }
+        $("#chart").data(questionText, newAnswer);
+        alterMood(this, newAnswer);
     });
-})(jQuery);
+}
