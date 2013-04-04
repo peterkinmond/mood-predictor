d8bcd5bdc49435b586054dc95b1128c8b33b8148
Peter Kinmond <peterkinmond@gmail.com>
2013-04-03 21:18:20 -0700

    Move js into file and remove global var in favour of data attribute

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,7 @@
 $(function() {
+    var initialMood = 8;   // Perfect mood is 10, start with 8
+    $('#chart').data('moodNumber', initialMood);
+
     setQuestionListener('office_temp');
     setQuestionListener('cups_of_coffee');
     setQuestionListener('time_of_day');
@@ -17,4 +20,26 @@ function setQuestionListener(questionText) {
         $("#chart").data(questionText, newAnswer);
         alterMood(this, newAnswer);
     });
+}
+
+function alterMood(checkbox, amount) {
+    var moodNumber = $('#chart').data('moodNumber');
+
+    if (checkbox.checked) {
+        moodNumber += amount;
+    }
+    else {
+        moodNumber -= amount;
+    }
+
+    $('#chart').data('moodNumber', moodNumber);
+
+    var offset = moodNumber;
+    if (moodNumber > 10) {
+        offset = 10;
+    }
+    else if (moodNumber <= 1) {
+        offset = 1.5;
+    }
+    redrawChart(offset * 36);
 }
